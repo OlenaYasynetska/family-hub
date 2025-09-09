e28f736033bd3c1ca74c441034d92786238d7545
Olena Ysynetska <olenayasynetska8@gmail.com>
2025-09-09 08:43:04 +0200

    CardColor

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'bg-pink-500',
+    'bg-yellow-400', 
+    'bg-blue-500',
+    'bg-orange-500',
+    'bg-green-500',
+    'bg-purple-500',
+    'text-white',
+    'text-gray-800'
+  ],
   theme: {
     extend: {},
   },
